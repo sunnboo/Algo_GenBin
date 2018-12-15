305264f54ad49e1def94b59855e112cb11825ee4
Touftoufe <touftoufeokami@gmail.com>
2018-12-15 23:38:24 +0100

    Update graph.m

--- graph.m
+++ graph.m
@@ -3,6 +3,13 @@ close all
 clc
 
 load('matlab.mat');
-plot(lambda,J,lambda,f(lambda,0.722296, 6562.565300, 10.774000, 0.218124),'r');
+Ymax = 0.722296;
+lambda0 = 6562.565300;
+d_lambdaL = 10.774000;
+Y0 = 0.218124;
+
+plot(lambda,J,lambda,f(lambda,Ymax, lambda0, d_lambdaL, Y0),'r'); 
+
+%la courbe en rouge represente celle generée par le programme et la bleu celle du prof
 figure(2)
-plot(lambda,JC,lambda,f(lambda,0.722296, 6562.565300, 10.774000, 0.218124),'r');
+plot(lambda,JC,lambda,f(lambda,Ymax, lambda0, d_lambdaL, Y0),'r'); 
